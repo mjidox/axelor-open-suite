84e60b7bc7a783b6af700be09035187cb9e41320
majda <m.elabdi@axelor.com>
2023-11-22 18:11:13 +0100

    local changes to test with

--- axelor-base/src/main/java/com/axelor/apps/base/web/MyController.java
+++ axelor-base/src/main/java/com/axelor/apps/base/web/MyController.java
@@ -1,2 +1,21 @@
-package com.axelor.apps.base.web;public class MyController {
+package com.axelor.apps.base.web;
+
+import com.axelor.apps.base.service.exception.TraceBackService;
+import com.axelor.inject.Beans;
+import com.axelor.message.db.Message;
+import com.axelor.message.service.MessageService;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+
+public class MyController {
+  public void fulltest(ActionRequest request, ActionResponse response) {
+    try {
+      Message mes = request.getContext().asType(Message.class);
+      String ter = Beans.get(MessageService.class).getFullEmailAddress(mes.getFromEmailAddress());
+      String tr=ter;
+      response.setValue("subject",tr);
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
+    }
+  }
 }
